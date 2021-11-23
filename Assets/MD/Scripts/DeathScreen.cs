38b04f23f8cdc00d84190b50e753bb4f7d1ea885
Michael <michaeldavy347@outlook.com>
2021-11-23 14:28:43 +1300

    Death screen take 2

---
+++ Assets/MD/Scripts/DeathScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+    public Text Message;
+  public List<string> Deathmessages = new List<string>();
+    private void Start()
+    {
+        Message.text = Deathmessages[Random.Range(0, 29)];
+    }
+
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+    }
+    
+
+    public void Menu ()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
