6de46b59af9100e2f6ed9655403c8fd25998ba7b
kyle <studio11keyuniverse@gmail.com>
2021-11-24 15:09:39 +1300

    Add Options To The Cool  Main

---
+++ Assets/KY/Op/VolumeControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+
+public class VolumeControl : MonoBehaviour
+{
+    public AudioMixer Mixer;
+    public string ParamName;
+    Slider _slider;
+
+    void Start()
+    {
+        _slider = GetComponent<Slider>();
+        float vol = PlayerPrefs.GetFloat(ParamName);
+        _slider.value = vol;
+        Change(vol);
+    }
+
+
+    public void Change(float value)
+    {
+        PlayerPrefs.SetFloat(ParamName, value);
+        value *= 80;
+        value -= 80;
+        Mixer.SetFloat(ParamName, value);
+        
+    }
+}
