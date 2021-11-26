1048e72bd5546efd26905c35c09ada8b27e3f26b
kyle <studio11keyuniverse@gmail.com>
2021-11-26 14:47:14 +1300

    Mouse Fix Maybe?

---
+++ Assets/KY/Scripts/Show.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Show : MonoBehaviour
+{
+
+        // Use this for initialization
+        void Start()
+        {
+            //Set Cursor to not be visible
+
+            Cursor.lockState = CursorLockMode.None;
+        }
+}
